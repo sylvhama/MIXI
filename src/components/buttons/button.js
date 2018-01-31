382d2efa998a9606cdfcec807f538a8fc780571f
Qi <qi.chen@ubisoft.com>
2018-01-31 17:24:41 -0500

    Bootstraped & initialized project

---
+++ src/components/buttons/button.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../../stylesheets/button.css"
+
+export class Button extends React.Component {
+    render() {     
+        return (
+            <div className="button">
+              <button type="button"></button>
+            </div>
+          )
+    }
+  }
